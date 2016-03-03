8e794be6346e6b65f8e439ea917b76a5e50c49fb
Alvin George <alvingeorge@fingents-mini-5.ftsindia.in>
2016-03-03 13:11:34 +0530

    changes

--- NetworkConnectivitySample/AppDelegate.swift
+++ NetworkConnectivitySample/AppDelegate.swift
@@ -12,10 +12,10 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
  var window: UIWindow?
-
+private var reachability:Reachability!;
 
  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-  // Override point for customization after application launch.
+
   return true
  }
 
